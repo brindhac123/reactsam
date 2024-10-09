38774c8721223be7f10742986200bb569a082afc
Brindha <brindha@nidrive.jp>
2024-10-09 17:52:14 +0530

    second commit

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,18 +1,41 @@
 import Car from './Car'
 import Apple from './Apple';
+import Gun from './Gun';
 
 function Garage(){
     //const brand="BMW";
     //const color="Black";
+    const isDoorOpened=true;
     const carInfo={brand:"BMW",color:"Black"}
+    //const carInfo={}
     const AppleInfo={typr:"Fuji",color:"Red"}
+    const carList= [
+      { brand :"BMW", color:"Red"},
+      { brand :"Ford", color:"Back"},
+      { brand :"Tesla", color:"Blue"},
+
+    ];
+    const numberList = [
+      1,2,3,4,5,6
+    ];
+
+    const showcarInfo = carInfo.brand !== undefined && carInfo.color !== undefined 
     return(
       <>
       <h2>Who is there</h2>
       {/* <Car brand={brand} color={color}/> */}
-      <Car carInfo={carInfo}/>
-
+      { showcarInfo && <Car carInfo={carInfo}/> }
+      
       <Apple AppleInfo={AppleInfo}/>
+      
+      {isDoorOpened?<h2>Garage door is opened</h2> : <h2>Garage door is closed</h2>}
+
+      <ul>
+        {carList.map((carInfo) => <li key={carInfo.brand} ><Car carInfo={carInfo}/> </li> ) }
+      </ul>
+      <ul>
+        {numberList.map((e,index) => <p key={index}>{e}</p> ) }
+      </ul>
       </>
     )
   }
